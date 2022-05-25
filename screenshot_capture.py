a5e5fe8b6477bb9f02f4946f3f66baef753ce858
anjor <anjor@umd.edu>
2022-05-25 13:18:26 +0100

    multiple

--- screenshot_capture.py
+++ screenshot_capture.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import os
 
 
 def grab_screenshot(driver: webdriver.Chrome, url: str, screenshot_name: str = None) -> bool:
@@ -15,6 +16,27 @@ def get_chrome_driver() -> webdriver.Chrome:
     return webdriver.Chrome('./chromedriver', options=options)
 
 
+def pngify(filename):
+    if not filename[-3:] == '.png':
+        return filename + '.png'
+    return filename
+
+
+def get_multiple_screenshots(urls: dict) -> None:
+    for url in urls:
+        tag, filename = urls[url]
+        os.makedirs(os.path.join(".", "screenshots", tag), exist_ok=True)
+        grab_screenshot(get_chrome_driver(), url, os.path.join(".", "screenshots", tag, pngify(filename)))
+
+
 if __name__ == '__main__':
-    URL = 'https://aws.amazon.com/s3/features'
-    grab_screenshot(get_chrome_driver(), URL, "test5.png")
+    urls_to_process = {
+        'https://aws.amazon.com/s3': ('aws', 'landing_page'),
+        'https://aws.amazon.com/s3/features': ('aws', 'features'),
+        'https://aws.amazon.com/s3/storage-classes': ('aws', 'storage-classes'),
+        'https://aws.amazon.com/s3/pricing': ('aws', 'pricing'),
+        'https://aws.amazon.com/s3/security': ('aws', 'security'),
+        'https://aws.amazon.com/s3/resources': ('aws', 'resources'),
+        'https://aws.amazon.com/s3/faqs': ('aws', 'faqs'),
+    }
+    get_multiple_screenshots(urls_to_process)
