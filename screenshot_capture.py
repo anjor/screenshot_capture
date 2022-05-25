065ea43aa23936d452bc3e1344f8b9f9063516b8
anjor <anjor@umd.edu>
2022-05-25 13:02:57 +0100

    fix

--- screenshot_capture.py
+++ screenshot_capture.py
@@ -1,14 +1,20 @@
 from selenium import webdriver
 
 
-def grab_screenshot(driver, url, screenshot_name=None):
+def grab_screenshot(driver: webdriver.Chrome, url: str, screenshot_name: str = None) -> bool:
     driver.get(url)
-    return driver.save_screenshot(screenshot_name or url + ".png")
+    S = lambda X: driver.execute_script('return document.body.parentNode.scroll' + X)
+    driver.set_window_size(S('Width'), S('Height'))  # May need manual adjustment
+    return driver.find_element_by_tag_name('body').screenshot(screenshot_name)
+
+
+def get_chrome_driver() -> webdriver.Chrome:
+    options = webdriver.ChromeOptions()
+    options.headless = True
+
+    return webdriver.Chrome('./chromedriver', options=options)
 
 
 if __name__ == '__main__':
-    DRIVER = './chromedriver'
     URL = 'https://aws.amazon.com/s3/features'
-    chrome_driver = webdriver.Chrome(DRIVER)
-    grab_screenshot(chrome_driver, URL, "test2.png")
-    chrome_driver.quit()
+    grab_screenshot(get_chrome_driver(), URL, "test5.png")
