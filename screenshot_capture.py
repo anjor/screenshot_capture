24da8b4f76aaf9449a03fe6e6d85be7eaefeb9af
anjor <anjor@umd.edu>
2022-05-25 10:08:55 +0100

    refactor

--- screenshot_capture.py
+++ screenshot_capture.py
@@ -1,9 +1,14 @@
 from selenium import webdriver
 
 
-DRIVER = 'chromedriver'
-URL = 'https://aws.amazon.com/s3'
-driver = webdriver.Chrome(DRIVER)
-driver.get(URL)
-screenshot = driver.save_screenshot('my_screenshot.png')
-driver.quit()
+def grab_screenshot(driver, url, screenshot_name=None):
+    driver.get(url)
+    return driver.save_screenshot(screenshot_name or url + ".png")
+
+
+if __name__ == '__main__':
+    DRIVER = './chromedriver'
+    URL = 'https://aws.amazon.com/s3/features'
+    chrome_driver = webdriver.Chrome(DRIVER)
+    grab_screenshot(chrome_driver, URL, "test2.png")
+    chrome_driver.quit()
